f68108eacea5475dee1a702c800d7f3ca50c53f1
zjensen262 <31410065+zjensen262@users.noreply.github.com>
2020-12-15 15:27:17 -0500

    test

--- models/token_classifier.py
+++ models/token_classifier.py
@@ -91,6 +91,26 @@ class TokenClassifier(object):
       self.Y_train = y_train
       self.Y_dev = y_dev
       self.Y_test = y_test
+      for x in self.X_train:
+        for y in x:
+          for z in y:
+            if z is None:
+              print('Found X Train')
+      for x in self.X_dev:
+        for y in x:
+          for z in y:
+            if z is None:
+              print('Found X Dev')
+      for x in self.Y_train:
+        for y in x:
+          for z in y:
+            if z is None:
+              print('Found Y Train')
+      for x in self.Y_dev:
+        for y in x:
+          for z in y:
+            if z is None:
+              print('Found Y Dev')
 
     else:
       self.X_train = None
@@ -127,15 +147,7 @@ class TokenClassifier(object):
       callbacks.append(
         EarlyStopping(monitor='val_loss', min_delta=0, patience=4, verbose=0, mode='auto')
       )
-    print(self.X_train is None)
-    print(self.Y_train is None)
-    print(batch_size is None)
-    print(num_epochs is None)
-    print(val_split is None)
-    print(self.X_dev is None)
-    print(self.Y_dev is None)
-    print(callbacks is None)
-    print(verbosity is None)
+
     self.model.fit(
       x=self.X_train,
       y=self.Y_train,
